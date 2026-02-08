4d931a58c1f0e5eb563238d5967bfb4da7e10ca6
AustinBozgoz <153233404+AustinBozgoz@users.noreply.github.com>
2026-02-08 13:57:33 -0500

    Update and rename main daily climograph.py to src/main.py

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ LCD_analyze=False #should the LCD folders in the NOAA_LCD_CSVs directory be anal
 quantity='all' #daily max quantity to be used. options are:'all','Tmax','Tavg','HImax','WBTmax','WBTavg','RHavg','RHmax'
 city='all' #give the exact name of the city as the folder is written within the NOAA_LCD_CSVs directory, or list 'all' to analyze all the cities in that folder
 stationID='' #the 3 letter id code for the chosen station of the selected city, can leave it blank if using all cities
-date='annual' #what day to analyze the climatography for, written in the format MO-DA (so january first is 01-01), or write 'annual' to analyze every day of the year excluding february 29th
+date='annual' #what day to analyze the climatography for, written in the format [MO,DA] (so january first is [1,1]), or write 'annual' to analyze every day of the year excluding february 29th
 year_range='all' #year range to include in the climograph analysis (written in format [start,end]), or choose 'all' to use all available years
 
 save_plots=True #save plots (True/False)
@@ -39,7 +39,9 @@ def main(LCD_analyze,quantity,city,stationID,date,year_range,save_plots,display_
     if LCD_analyze==True:
         logging.info('Beginning LCD Analysis')
         LCD_hourly_daily_max.LCD_hourly_daily_max(current_directory+'\\NOAA_LCD_CSVs\\')
+    logging.info('Beginning Analysis')
     daily_climograph_trend_analysis.daily_climograph_trend_analysis(date,city,stationID+' LCD',year_range,quantity,current_directory+'\\NOAA_LCD_CSVs\\',save_plots,display_plots)
+    logging.info('Script Complete')
     return
 
 
